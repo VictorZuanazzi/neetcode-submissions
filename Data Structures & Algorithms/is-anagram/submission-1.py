4d329967d6637595d2ab680025bc8e85cd034218
Victor <31482851+VictorZuanazzi@users.noreply.github.com>
2026-06-05 10:28:47 +0200

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.py
@@ -0,0 +1,19 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        
+        def _build_dict(s):
+            dict_s = {}
+            for c in s:
+                dict_s[c] = 1 + dict_s.get(c, 0)
+            return dict_s
+        
+        dict_s = _build_dict(s)
+        dict_t = _build_dict(t)
+
+        for char, freq in dict_s.items():
+            if dict_t.get(char, 0) != freq:
+                return False
+        
+        return True
